a54ab575d45753a9df2e624672b1b0a77d157e6e
Arcane <ngwaikeet@gmail.com>
2026-04-04 03:47:21 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/logic/commands/DeleteContactCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteContactCommandTest.java
@@ -7,7 +7,10 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.*;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.CARL;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
